c07b60d1962abb6f3e88222b1dfbe0bf90cd5d5a
Vladyslav <itkuf00@gmail.com>
2026-09-25 17:26:42 +0300

    Add SolidIngredient.cpp definition

---
+++ src/core/SolidIngredient.cpp
@@ -0,0 +1,14 @@
+#include "core/SolidIngredient.h"
+#include <iostream>
+namespace core {
+    SolidIngredient::SolidIngredient(const std::string &name, double amount, const std::string &expirationDate)
+        : Ingredient(name,amount,expirationDate) {}
+
+    std::string SolidIngredient::getUnit() const {
+        return "g"; // Grams
+    }
+
+    void SolidIngredient::printInfo() const {
+        std::cout << "[Solid] " << name << ": " << amount << getUnit()
+        << "(expires: " << expirationDate << " )\n";
+    }
