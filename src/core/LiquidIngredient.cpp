6a0040fef671ff8c856d1c691431ca082c562bf5
Vladyslav <itkuf00@gmail.com>
2026-09-25 17:56:37 +0300

    Add LiquidIngredient.cpp definition

---
+++ src/core/LiquidIngredient.cpp
@@ -0,0 +1,17 @@
+#include "core/LiquidIngredient.h"
+#include <iostream>
+
+namespace core {
+    LiquidIngredient::LiquidIngredient(const std::string &name, double amount, const std::string &expirationDate)
+        : Ingredient(name, amount, expirationDate) {
+    }
+
+    std::string LiquidIngredient::getUnit() const {
+        return "ml\n";
+    }
+
+    void LiquidIngredient::printInfo() const {
+        std::cout << "[Liquid ]" << name << ": " << amount << getUnit()
+                << "(expires " << expirationDate << ") \n";
+    }
+}
